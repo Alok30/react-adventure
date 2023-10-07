7f805c8a73d2d9d0cee0d7c38e138051405ecef4
AlokD <alokje2013@gmail.com>
2023-10-07 21:08:03 +0530

    Test Cookie

--- src/constant.js
+++ src/constant.js
@@ -1,2 +1,2 @@
-// export const BASE_URL='https://sapient-backend-8f2f875f655c.herokuapp.com';
-export const BASE_URL='http://localhost:9000';
+export const BASE_URL='https://sapient-backend-8f2f875f655c.herokuapp.com';
+// export const BASE_URL='http://localhost:9000';
